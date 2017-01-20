00a4d4da56d33ffb10d829f2b878f56fd4cbee20
Michael Lennartz <michael@dubspeed.de>
2017-01-20 19:32:12 +0100

    added scripts folder and an asset provider

---
+++ Assets/Scripts/System/SimpleAssetProvider.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using System;
+
+public class AssetProvider : MonoBehaviour
+{
+
+    public event System.Action<GameObject> OnAssetLoaded = (rootObjectJustLoaded) => {};
+
+    Dictionary<string, List<GameObject>> RegisteredAssets = new Dictionary<string, List<GameObject>> ();
+
+    public void LoadAssetAsync (string assetId)
+    {
+        StartCoroutine (LoadAsset (assetId)); 
+    }
+
+    public IEnumerator LoadAsset (string assetId)
+    {
+        yield return SceneManager.LoadSceneAsync (assetId, LoadSceneMode.Additive); 
+        var scene = SceneManager.GetSceneByName (assetId);
+
+        if (scene.IsValid ()) {
+            // find the first object which is not of type "SceneContext" and register that
+            // enforces the game convention to have a GO under a SceneContext with the same name as the scene
+
+            var sceneObjects = scene.GetRootGameObjects ();
+            if (sceneObjects [0].name != "SceneContext") {
+                throw new Exception ("AssetProvider: Please have a 'SceneContext' as a scene root: " + assetId + " / " + sceneObjects [0].name);
+            }
+
+            var rootObject = sceneObjects [0].transform.GetChild (0).gameObject;
+            if (rootObject.name != assetId) {
+                throw new Exception ("AssetProvider: Please name your scene rootObject and scene file the same: " + assetId);
+            }
+
+            RegisterAsset (rootObject);
+        }
+    }
+
+    public void RegisterAsset (GameObject rootObject)
+    {
+        if (!RegisteredAssets.ContainsKey (rootObject.name)) {
+            RegisteredAssets.Add (rootObject.name, new List<GameObject> ()); 
+        }
+        RegisteredAssets [rootObject.name].Add (rootObject); 
+
+        OnAssetLoaded (rootObject); 
+    }
+
+    public void UnloadAsset (string id)
+    {
+        StartCoroutine (Co_UnloadAssets (id)); 
+    }
+
+    IEnumerator Co_UnloadAssets (string id)
+    {
+        // we need to wait one frame since it could be that this unload was triggered 
+        // by an physic event - and there is a bug in Unity that makes unity hang then 
+        yield return new WaitForEndOfFrame (); 
+        if (RegisteredAssets.ContainsKey (id)) {
+            foreach (GameObject asset in this.RegisteredAssets[id]) {
+                SceneManager.UnloadSceneAsync (asset.name); 
+            }
+            RegisteredAssets [id].Clear (); 
+        }
+    }
+
+    public GameObject GetFirstAsset (string id)
+    {
+        if (!RegisteredAssets.ContainsKey (id)) {
+            throw new Exception ("Asset " + id + " not registered in AssetProvider");
+        }
+        return RegisteredAssets [id] [0];
+    }
+}
