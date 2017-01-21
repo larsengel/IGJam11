edab9a0e8345d433ef0a55c9f2d264dd88f211aa
Lars <lars@l-engel.de>
2017-01-21 09:54:13 +0100

    Add left sign to Audience

---
+++ Assets/Wave/Scripts/Audience/Sign.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sign : MonoBehaviour {
+    private void Start()
+    {
+        //GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+
+    void Update () {
+        var rotation = Quaternion.LookRotation(new Vector3(0,0,-245.348f));
+        transform.rotation = rotation;
+    }
+}
