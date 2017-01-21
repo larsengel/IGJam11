7627efca94bf36dd9fc8860826b55a2bf4320ea2
Lars <lars@l-engel.de>
2017-01-21 23:03:11 +0100

    Fix for Schild

---
+++ Assets/Wave/Scripts/Audience/SchildAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SchildAnimation : MonoBehaviour {
+
+    private void OnEnable()
+    {
+        transform.GetComponent<Animator>().SetTrigger("start");
+    }
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
