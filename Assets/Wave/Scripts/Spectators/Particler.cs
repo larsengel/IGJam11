ae2a2bfdf2de33dd7de7516308d3fa90be0a2b75
Michael Lennartz <michael@dubspeed.de>
2017-01-21 18:05:54 +0100

    added particles effects for happy and upset

---
+++ Assets/Wave/Scripts/Spectators/Particler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particler : MonoBehaviour
+{
+
+    public GameObject HappyParticlePrefab;
+    public GameObject UpsetParticlePrefab;
+
+    public void ShowHappy ()
+    {
+        var pos = transform.position;
+        var particle = Instantiate (HappyParticlePrefab, pos, Quaternion.identity);
+        Destroy (particle, 2.0f);
+    }
+
+    public void ShowUpset ()
+    {
+        var pos = transform.position;
+        var particle = Instantiate (UpsetParticlePrefab, pos, Quaternion.identity);
+        Destroy (particle, 2.0f);
+    }
+}
