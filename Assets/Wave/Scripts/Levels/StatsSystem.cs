5f9bd6e54e4ccab5fa67cb05a9867433cac5aef5
Max <max@ximga.com>
2017-01-21 17:39:24 +0100

    avarge  wave points

---
+++ Assets/Wave/Scripts/Levels/StatsSystem.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Wave.Levels
+{
+    public class StatsSystem : MonoBehaviour
+    {
+        public DefeatSystem defeatSystem;
+        public LevelSystem levelSystem;
+
+        List<float> pointsHistory = new List<float>();
+        public float AvaragePoints
+        {
+            get
+            {
+                if (pointsHistory.Count == 0) return 0;
+                return pointsHistory.Sum() / pointsHistory.Count;
+            }
+        }
+
+        private void OnEnable()
+        {
+            levelSystem.LevelStarted += this.OnLevelStarted;
+            levelSystem.LevelFinished += this.OnLevelFinished;
+        }
+
+        private void OnLevelStarted()
+        {
+            Debug.Log("OnLevelStarted");
+            StartCoroutine(ComputeAvarageRatio());
+            pointsHistory = new List<float>();
+        }
+
+        private void OnLevelFinished()
+        {
+            Debug.Log("OnLevelFinished");
+            StopAllCoroutines();
+        }
+
+        private IEnumerator ComputeAvarageRatio()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(1);
+                Debug.Log(defeatSystem.WaveRatio + "-> defeatSystem.WaveRatio");
+                pointsHistory.Add(defeatSystem.WaveRatio);
+            }
+        }
+
+
+    }
+}
