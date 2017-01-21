6e97dc13cd659aa608c0c12d475008bcfa61b954
Lars <lars@l-engel.de>
2017-01-21 13:43:20 +0100

    Example texts for signs

--- Assets/Wave/Scripts/Audience/SignText.cs
+++ Assets/Wave/Scripts/Audience/SignText.cs
@@ -2,8 +2,11 @@
 
 public class SignText : MonoBehaviour
 {
+    public string[] Texts;
+
     void Start()
     {
         GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<TextMesh>().text = Texts[Random.Range(0, Texts.Length)];
     }
 }
