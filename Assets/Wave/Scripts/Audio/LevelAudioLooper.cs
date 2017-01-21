98d69a6c5e2700014ba2a902e011b3732b1f7b7c
Michael Lennartz <michael@dubspeed.de>
2017-01-21 15:55:38 +0100

    added sound to levels

---
+++ Assets/Wave/Scripts/Audio/LevelAudioLooper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(AudioSource))]
+    
+public class LevelAudioLooper : MonoBehaviour
+{
+
+    public AudioClip Intro;
+    public AudioClip Loop;
+
+    AudioSource AudioSource;
+
+
+    void Awake ()
+    {
+        AudioSource = GetComponent<AudioSource> ();
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        AudioSource.clip = Intro;
+        AudioSource.Play ();
+    }
+	
+    // Update is called once per frame
+    void Update ()
+    {
+        if (!AudioSource.isPlaying) {
+            AudioSource.clip = Loop;
+            AudioSource.Play ();
+        }
+    }
+}
