d9bb934f5c9bf5172528561147221e80b86447c1
Marco Behnke <marco.behnke@innogames.com>
2017-01-22 11:45:45 +0100

    add shaker bound to stamina bar

--- Assets/Wave/Scripts/Levels/UIStaminaBar.cs
+++ Assets/Wave/Scripts/Levels/UIStaminaBar.cs
@@ -1,25 +1,30 @@
 ﻿namespace Wave.Levels
 {
-    using UnityEngine;
-    using UnityEngine.UI;
+	using UnityEngine;
+	using UnityEngine.UI;
 
-    public class UIStaminaBar : MonoBehaviour
-    {
-        public Image FillImage;
+	public class UIStaminaBar : MonoBehaviour
+	{
+		public Image FillImage;
 
-        private DefeatSystem defeatSystem;
+		private DefeatSystem defeatSystem;
 
-        private void Start()
-        {
-            this.defeatSystem = FindObjectOfType<DefeatSystem>();
-        }
+		private void Start ()
+		{
+			this.defeatSystem = FindObjectOfType<DefeatSystem> ();
+		}
 
-        private void Update()
-        {
-            if (this.defeatSystem != null && this.FillImage != null)
-            {
-                this.FillImage.fillAmount = this.defeatSystem.RemainingStaminaRatio;
-            }
-        }
-    }
+		private void Update ()
+		{
+			if (this.defeatSystem != null && this.FillImage != null) {
+				this.FillImage.fillAmount = this.defeatSystem.RemainingStaminaRatio;
+
+				Shaker shaker = Main.Instance.mainCamera.GetComponent<Shaker> ();
+				if (this.FillImage.fillAmount < 0.5f) {
+					shaker.shakeAmount = 1 - this.FillImage.fillAmount;
+					shaker.StartShake ();
+				}
+			}
+		}
+	}
 }
