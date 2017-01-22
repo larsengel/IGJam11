7d4a1bde75612314d98bef1cb0b323fc50b1fbc3
Marco Behnke <marco.behnke@innogames.com>
2017-01-22 12:39:15 +0100

    mute sound for the make happy loop to avoid volume peak

--- Assets/Wave/Scripts/Spectators/SpectatorSystem.cs
+++ Assets/Wave/Scripts/Spectators/SpectatorSystem.cs
@@ -1,52 +1,64 @@
 ﻿namespace Wave.Spectators
 {
-    using System.Collections.Generic;
-    using System.Linq;
+	using System.Collections.Generic;
+	using System.Linq;
 
-    using UnityEngine;
+	using UnityEngine;
 
-    using Wave.Levels;
+	using Wave.Levels;
 
-    public class SpectatorSystem : MonoBehaviour
-    {
-        public LevelSystem LevelSystem;
+	public class SpectatorSystem : MonoBehaviour
+	{
+		public LevelSystem LevelSystem;
 
-        public List<Spectator> Spectators;
+		public List<Spectator> Spectators;
 
-        [ContextMenu ("Find Spectators")]
-        private void FindSpectators ()
-        {
-            this.Spectators = FindObjectsOfType<Spectator> ().ToList ();
-        }
+		[ContextMenu ("Find Spectators")]
+		private void FindSpectators ()
+		{
+			this.Spectators = FindObjectsOfType<Spectator> ().ToList ();
+		}
 
-        private void OnDisable ()
-        {
-            this.LevelSystem.LevelStarted -= this.OnLevelStarted;
-            this.LevelSystem.LevelFinished -= this.OnLevelFinished;
-        }
+		private void OnDisable ()
+		{
+			this.LevelSystem.LevelStarted -= this.OnLevelStarted;
+			this.LevelSystem.LevelFinished -= this.OnLevelFinished;
+		}
 
-        private void OnEnable ()
-        {
-            this.LevelSystem.LevelStarted += this.OnLevelStarted;
-            this.LevelSystem.LevelFinished += this.OnLevelFinished;
-        }
+		private void OnEnable ()
+		{
+			this.LevelSystem.LevelStarted += this.OnLevelStarted;
+			this.LevelSystem.LevelFinished += this.OnLevelFinished;
+		}
 
-        private void OnLevelFinished ()
-        {
-            this.Spectators = null;
-        }
+		private void OnLevelFinished ()
+		{
+			this.Spectators = null;
+		}
 
-        private void OnLevelStarted ()
-        {
-            this.FindSpectators ();
-            this.MakeSpectatorsWave ();
-        }
+		private void OnLevelStarted ()
+		{
+			this.FindSpectators ();
+			this.MakeSpectatorsWave ();
+		}
 
-        private void MakeSpectatorsWave ()
-        {
-            foreach (var spectator in this.Spectators) {
-                spectator.MakeHappy ();
-            }
-        }
-    }
+		private void MakeSpectatorsWave ()
+		{
+			foreach (var spectator in this.Spectators) {
+				AudioSource audioSource = spectator.GetComponent<AudioSource> ();
+				audioSource.volume = 0.0f;
+				spectator.MakeHappy ();
+			}
+
+			Invoke ("EnableSoundOnSpectators", 1.0f);
+		}
+
+		private void EnableSoundOnSpectators ()
+		{
+			foreach (var spectator in this.Spectators) {
+				AudioSource audioSource = spectator.GetComponent<AudioSource> ();
+				audioSource.volume = 1.0f;
+			}
+		}
+	}
 }
