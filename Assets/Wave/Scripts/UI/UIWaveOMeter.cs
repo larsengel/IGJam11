36830007d2ee56b0e70f5b927edd54340bd6ae0d
Michael Lennartz <michael@dubspeed.de>
2017-01-22 10:19:31 +0100

    added new waveometer

--- Assets/Wave/Scripts/UI/UIWaveOMeter.cs
+++ Assets/Wave/Scripts/UI/UIWaveOMeter.cs
@@ -4,29 +4,29 @@ using Wave.Levels;
 
 public class UIWaveOMeter : MonoBehaviour
 {
-    public RectTransform Pointer;
+    public Transform Pointer;
 
-    [Range(0.0f, 1.0f)]
+    [Range (0.0f, 1.0f)]
     public float Value;
 
     private DefeatSystem defeatSystem;
 
-    private void OnDrawGizmos()
+    private void OnDrawGizmos ()
     {
-        //Update ();
+        Update ();
     }
 
-    private void Start()
+    private void Start ()
     {
-        this.defeatSystem = FindObjectOfType<DefeatSystem>();
+        this.defeatSystem = FindObjectOfType<DefeatSystem> ();
     }
 
     // 0 ist 90 und 1 is -90
-    private void Update()
+    private void Update ()
     {
-        this.Value = this.defeatSystem != null ? this.defeatSystem.WaveRatio : 0.5f;
+        //this.Value = this.defeatSystem != null ? this.defeatSystem.WaveRatio : 0.5f;
 
         float euler_angle = 90.0f - (180.0f * this.Value);
-        this.Pointer.rotation = Quaternion.Euler(new Vector3(0, 0, euler_angle));
+        this.Pointer.rotation = Quaternion.Euler (new Vector3 (0, 0, euler_angle));
     }
 }
