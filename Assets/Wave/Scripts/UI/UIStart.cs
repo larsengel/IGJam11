3f9725425972a22686c328e95f1fe5fc5bc6e2ad
Christian Oeing <christian.oeing@slashgames.org>
2017-01-22 12:25:14 +0100

    Add quit button to main menu

---
+++ Assets/Wave/Scripts/UI/UIStart.cs
@@ -0,0 +1,12 @@
+﻿namespace Wave.UI
+{
+    using UnityEngine;
+
+    public class UIStart : MonoBehaviour
+    {
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
