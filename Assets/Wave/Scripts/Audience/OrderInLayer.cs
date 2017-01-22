50c5b231e7f77dbefdb54da67838bdbdde0f9946
Lars <lars@l-engel.de>
2017-01-22 10:45:44 +0100

    Fix order index for signs really!!

--- Assets/Wave/Scripts/Audience/OrderInLayer.cs
+++ Assets/Wave/Scripts/Audience/OrderInLayer.cs
@@ -4,39 +4,4 @@ using Spriter2UnityDX;
 using UnityEngine;
 
 public class OrderInLayer : MonoBehaviour {
-
-    static int Offset = 0;
-
-	// Use this for initialization
-	void Start ()
-	{
-	    Offset += 1000;
-
-	    foreach (var renderer in GetComponentsInChildren<Renderer>())
-	    {
-	        var order = renderer.sortingOrder + Offset;
-	        renderer.sortingOrder = order;
-	    }
-
-	    Search(transform, "schild_ani_right").GetComponent<EntityRenderer>().SortingOrder += Offset;
-	    Search(transform, "schild_ani_left").GetComponent<EntityRenderer>().SortingOrder += Offset;
-
-	}
-
-    public static Transform Search (Transform target, string name)
-    {
-        if (target.name == name) {
-            return target;
-        }
-
-        for (int i = 0; i < target.childCount; ++i) {
-            var result = Search (target.GetChild (i), name);
-
-            if (result != null) {
-                return result;
-            }
-        }
-
-        return null;
-    }
 }
