79ca5f779dd3107d785a59a42df30c02ae584737
Michael Lennartz <michael@dubspeed.de>
2017-01-22 13:04:26 +0100

    fix tutorial dude halo

--- Assets/Wave/Scripts/Spectators/Particler.cs
+++ Assets/Wave/Scripts/Spectators/Particler.cs
@@ -1,64 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Wave.Levels;
 
 public class Particler : MonoBehaviour
 {
 
-	public GameObject HappyParticlePrefab;
-	public GameObject UpsetParticlePrefab;
-	public GameObject HappyHaloPrefab;
+    public GameObject HappyParticlePrefab;
+    public GameObject UpsetParticlePrefab;
+    public GameObject HappyHaloPrefab;
 
-	public AudioClip PowerUpSound;
-	public AudioClip HappySound;
+    public AudioClip PowerUpSound;
+    public AudioClip HappySound;
 
-	public float Duration { get; set; }
+    private float duration;
 
-	public HappyHalo HappyHalo;
+    public float Duration {
+        get {
+            if (duration == 0.0f) {
+                duration = FindObjectOfType<LevelSystem> ().CurrentConfiguration.PersuadeDuration;
+            }
+            return duration;
+        }
+        set {
+            duration = value;  
+        }
+    }
 
-	public HappyHalo CreateHalo ()
-	{
-		if (HappyHalo != null) {
-			return HappyHalo;
-		}
+    public HappyHalo HappyHalo;
 
-		HappyHalo = Instantiate (HappyHaloPrefab, Vector3.zero, Quaternion.identity, transform).GetComponent<HappyHalo> ();
-		HappyHalo.transform.localPosition = Vector3.zero;
-		HappyHalo.SpeedFactor = Duration;
+    public HappyHalo CreateHalo ()
+    {
+        if (HappyHalo != null) {
+            return HappyHalo;
+        }
 
-		var AudioSource = GetComponent<AudioSource> ();
-		AudioSource.clip = PowerUpSound;
-		AudioSource.Play ();
+        HappyHalo = Instantiate (HappyHaloPrefab, Vector3.zero, Quaternion.identity, transform).GetComponent<HappyHalo> ();
+        HappyHalo.transform.localPosition = Vector3.zero;
+        HappyHalo.SpeedFactor = Duration;
 
-		return HappyHalo;
-	}
+        var AudioSource = GetComponent<AudioSource> ();
+        AudioSource.clip = PowerUpSound;
+        AudioSource.Play ();
 
-	public void DestroyHalo ()
-	{
-		if (HappyHalo != null) {
-			Destroy (HappyHalo.gameObject);    
-			HappyHalo = null;
-		}
+        return HappyHalo;
+    }
 
-		var AudioSource = GetComponent<AudioSource> ();
-		AudioSource.Stop ();
-	}
+    public void DestroyHalo ()
+    {
+        if (HappyHalo != null) {
+            Destroy (HappyHalo.gameObject);    
+            HappyHalo = null;
+        }
 
-	public void ShowHappy ()
-	{
-		var pos = transform.position;
-		var particle = Instantiate (HappyParticlePrefab, pos, Quaternion.identity);
-		Destroy (particle, 2.0f);
+        var AudioSource = GetComponent<AudioSource> ();
+        AudioSource.Stop ();
+    }
 
-		var AudioSource = GetComponent<AudioSource> ();
-		AudioSource.clip = HappySound;
-		AudioSource.Play ();
-	}
+    public void ShowHappy ()
+    {
+        var pos = transform.position;
+        var particle = Instantiate (HappyParticlePrefab, pos, Quaternion.identity);
+        Destroy (particle, 2.0f);
 
-	public void ShowUpset ()
-	{
-		var pos = transform.position;
-		var particle = Instantiate (UpsetParticlePrefab, pos, Quaternion.identity);
-		Destroy (particle, 2.0f);
-	}
+        var AudioSource = GetComponent<AudioSource> ();
+        AudioSource.clip = HappySound;
+        AudioSource.Play ();
+    }
+
+    public void ShowUpset ()
+    {
+        var pos = transform.position;
+        var particle = Instantiate (UpsetParticlePrefab, pos, Quaternion.identity);
+        Destroy (particle, 2.0f);
+    }
 }
