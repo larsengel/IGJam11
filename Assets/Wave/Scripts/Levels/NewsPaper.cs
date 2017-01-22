0db5da0c8ef127e8bb53cd5ec2280d3428b8c23f
Steffen Hees <s.hees@gmx.de>
2017-01-22 11:49:05 +0100

    Added news outlets to news and put the bad success news to 75 percent

--- Assets/Wave/Scripts/Levels/NewsPaper.cs
+++ Assets/Wave/Scripts/Levels/NewsPaper.cs
@@ -6,9 +6,9 @@ using UnityEngine.UI;
 public class NewsPaper : MonoBehaviour
 {
     public Text headline;
-    string winHigh = "Electors remain faithful to Trump and select him as official winner";
-    string winLow = "ZZZZZZzzzzz. Audience falling asleep at low energy event. Trump’s highly anticipated speech gets subdued response.";
-    string loose = "When Trump hit a rough patch in his approval ratings, he said the rules were rigged against him and labeled the whole game a \"scam\" perpetrated by shaddy developers.";
+    string winHigh = "CNN: Electors remain faithful to Trump and select him as official winner";
+    string winLow = "FakeNews: ZZZZZZzzzzz. Audience falling asleep at low energy event. Trump’s highly anticipated speech gets subdued response.";
+    string loose = "FoxNews: When Trump hit a rough patch in his approval ratings, he said the rules were rigged against him and labeled the whole game a \"scam\" perpetrated by shaddy developers.";
 
 
     public void Init(bool isWinner, float score)
@@ -17,7 +17,7 @@ public class NewsPaper : MonoBehaviour
         var text = loose;
         if (isWinner)
         {
-            text = score >= 0.5 ? winHigh : winLow;
+            text = score >= 0.75 ? winHigh : winLow;
         }
         headline.text = text;
     }
